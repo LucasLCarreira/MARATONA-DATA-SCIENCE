243530162b6224435db5d9cac2f4f0f90a5d3694
Lucas Lasevicius Carreira <63404759+LucasLCarreira@users.noreply.github.com>
2020-05-11 16:20:48 -0300

    Delete 010_LISTA1.py

--- PYTHON/003_STRINGS/010_LISTA1.py
+++
@@ -1,6 +0,0 @@
-# CRIE UMA LISTA VAZIA E RECEBA UM INPUT DE TRES NUMEROS
-lista = []
-lista.append(int(input('Digite o primeiro número: ')))
-lista.append(int(input('Digite o segundo número: ')))
-lista.append(int(input('Digite o terceiro número: ')))
-print(lista)
