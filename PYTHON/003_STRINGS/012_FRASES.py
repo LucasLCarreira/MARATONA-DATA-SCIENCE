4566bb1162a9634e146f2f54bac37d803a77849b
Lucas Lasevicius Carreira <63404759+LucasLCarreira@users.noreply.github.com>
2020-05-11 16:21:02 -0300

    Delete 012_FRASES.py

--- PYTHON/003_STRINGS/012_FRASES.py
+++
@@ -1,7 +0,0 @@
-# CRIE UMA LISTA VAZIA E RECEBA UM INPUT DE TRES FRASES
-# ORDENE A LISTA EM ORDEM ALFABETICA DECRESCENTE
-lista = []
-lista.append(str(input('Digite a primeira frase: ')).upper())
-lista.append(str(input('Digite a segunda frase: ')).upper())
-lista.append(str(input('Digite a terceira frase: ')).upper())
-print(sorted(lista, reverse=True))
