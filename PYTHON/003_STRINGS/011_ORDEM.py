58d05fd2df207c9caedbda5d43f931bb3dc2c965
Lucas Lasevicius Carreira <63404759+LucasLCarreira@users.noreply.github.com>
2020-05-11 16:20:55 -0300

    Delete 011_ORDEM.py

--- PYTHON/003_STRINGS/011_ORDEM.py
+++
@@ -1,6 +0,0 @@
-# ORDENE A LISTA
-lista = []
-lista.append(int(input('Digite o primeiro número: ')))
-lista.append(int(input('Digite o segundo número: ')))
-lista.append(int(input('Digite o terceiro número: ')))
-print(f'A lista ordenada de forma crescente é {sorted(lista)}')
